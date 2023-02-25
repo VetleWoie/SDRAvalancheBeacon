6fccfa42d0a05fbd6259cfb64c4d54a8829d13d4
Vetle Hofsøy-Woie <vho023@uit.no>
2023-02-25 11:54:08 +0100

    :construction: Prettify plotting of specgram

--- src/specgram.py
+++ src/specgram.py
@@ -24,17 +24,19 @@ if __name__ == "__main__":
     alpha = np.fromfile(f"{datafolder}/alpha.bin",dtype=np.complex64)
     beta = np.fromfile(f"{datafolder}/beta.bin",dtype=np.complex64)
 
+    fig.suptitle(sys.argv[2])
+
     
     spec, freqs, t, im = ax[0][0].specgram(alpha,NFFT=8192,Fs=768.1e3,Fc=457e3,noverlap=2048)
     ax[0][0].set_title("Spectogram Alpha")
     ax[0][0].set_ylabel("frequency/hz")
-    ax[0][0].set_xlabel("time/s")
+    ax[0][0].set_xticklabels([])
     db_spec = 10*np.log10(spec)
     signaltonoise = np.array([snr(f, 8192//2, 20,20,"dbm") for f in db_spec.T])
     ax[0][1].plot(t,signaltonoise)
     ax[0][1].set_title("SNR Alpha")
     ax[0][1].set_ylabel("db")
-    ax[0][1].set_xlabel("time/s")
+    ax[0][1].set_xticklabels([])
 
     spec, freqs, t, im = ax[1][0].specgram(beta,NFFT=8192,Fs=768.1e3,Fc=457e3,noverlap=2048)
     ax[1][0].set_title("Spectogram Beta")
@@ -47,5 +49,7 @@ if __name__ == "__main__":
     ax[1][1].set_ylabel("db")
     ax[1][1].set_xlabel("time/s")
     
+    if len(sys.argv) > 3:
+        plt.savefig(sys.argv[3],bbox_inches='tight')
     plt.show()
 
