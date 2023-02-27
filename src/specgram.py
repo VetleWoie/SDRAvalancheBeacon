af9e12a4c835f3d73a1e4a399f08b201972afcac
Vetle Hofsøy-Woie <vho023@uit.no>
2023-02-27 15:28:28 +0100

    :art: use methods from utilsfile instead

--- src/specgram.py
+++ src/specgram.py
@@ -2,21 +2,8 @@ import numpy as np
 from scipy import signal
 from matplotlib import pyplot as plt
 import sys
+from utils import snr
 
-def snr(spec,signal_center, signal_width, noise_width, input_type='linear'):
-    signal_min = signal_center-signal_width
-    signal_max = signal_center+signal_width
-    signal_amp = np.max(spec[signal_min:signal_max])
-    noise_amp_left = np.max(spec[signal_min-noise_width:signal_min])
-    noise_amp_right = np.max(spec[signal_max:signal_max+noise_width])
-    noise_amp = noise_amp_left if noise_amp_left > noise_amp_right else noise_amp_right
-    if input_type == 'linear':
-        return 10*np.log10(signal_amp/noise_amp)
-    elif input_type == 'dbm':
-        return signal_amp-noise_amp
-    else:
-        raise ValueError(f"Unkown input type: \"{input_type}\", has to be one of [\"dbm\", \"linear\"]")
-    
 if __name__ == "__main__":
     datafolder = sys.argv[1]
 
@@ -27,7 +14,7 @@ if __name__ == "__main__":
     fig.suptitle(sys.argv[2])
 
     
-    spec, freqs, t, im = ax[0][0].specgram(alpha,NFFT=8192,Fs=768.1e3,Fc=457e3,noverlap=2048)
+    spec, freqs, t, im = ax[0][0].specgram(alpha,NFFT=8192,Fs=768e3,Fc=457e3,noverlap=2048, window=signal.windows.flattop(8192))
     ax[0][0].set_title("Spectogram Alpha")
     ax[0][0].set_ylabel("frequency/hz")
     ax[0][0].set_xticklabels([])
@@ -38,7 +25,7 @@ if __name__ == "__main__":
     ax[0][1].set_ylabel("db")
     ax[0][1].set_xticklabels([])
 
-    spec, freqs, t, im = ax[1][0].specgram(beta,NFFT=8192,Fs=768.1e3,Fc=457e3,noverlap=2048)
+    spec, freqs, t, im = ax[1][0].specgram(beta,NFFT=8192,Fs=768e3,Fc=457e3,noverlap=2048, window=signal.windows.flattop(8192))
     ax[1][0].set_title("Spectogram Beta")
     ax[1][0].set_ylabel("frequency/hz")
     ax[1][0].set_xlabel("time/s")
